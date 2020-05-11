e547f66c988b89ed989afd5e8484a55bcdab10d2
scy <1294974924@qq.com>
2020-05-11 09:57:06 +0800

    0511

--- layers.py
+++ layers.py
@@ -11,7 +11,7 @@ class MyLayer(nn.Module):
         self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))
         nn.init.xavier_uniform_(self.W.data, gain=1.414)
 
-        self.attention = entropy_attention.cuda()
+        self.attention = nn.Parameter(entropy_attention.cuda())
 
         # self.attention= nn.Parameter(entropy_attention)
         # nn.init.xavier_uniform_(self.attention.data, gain=1.414)
