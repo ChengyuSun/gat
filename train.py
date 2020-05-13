3994d07b14fe54415c10416c723a8d8a073730d0
scy <1294974924@qq.com>
2020-05-13 16:26:57 +0800

    0513

--- train.py
+++ train.py
@@ -185,24 +185,24 @@ for epoch in range(args.epochs):
     if bad_counter == args.patience:
         break
 
-    files = glob.glob('*.pkl')
-    for file in files:
-        epoch_nb = int(file.split('.')[0])
-        if epoch_nb < best_epoch:
-            os.remove(file)
-
-files = glob.glob('*.pkl')
-for file in files:
-    epoch_nb = int(file.split('.')[0])
-    if epoch_nb > best_epoch:
-        os.remove(file)
+#     files = glob.glob('*.pkl')
+#     for file in files:
+#         epoch_nb = int(file.split('.')[0])
+#         if epoch_nb < best_epoch:
+#             os.remove(file)
+#
+# files = glob.glob('*.pkl')
+# for file in files:
+#     epoch_nb = int(file.split('.')[0])
+#     if epoch_nb > best_epoch:
+#         os.remove(file)
 
 print("Optimization Finished!")
 print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
 
-# Restore best model
-print('Loading {}th epoch'.format(best_epoch))
-model.load_state_dict(torch.load('{}.pkl'.format(best_epoch)))
+# # Restore best model
+# print('Loading {}th epoch'.format(best_epoch))
+# model.load_state_dict(torch.load('{}.pkl'.format(best_epoch)))
 
 # Testing
 compute_test()
